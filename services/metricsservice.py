1fecab672984784fa33ef07420b7a1647c5e4f49
Joaci Morais <jmorais@vtinova.com.br>
2022-01-20 15:51:42 -0400

    fix(metricsservice): fix metric url and prescription_id after post metrics to service

--- services/metricsservice.py
+++ services/metricsservice.py
@@ -1,3 +1,5 @@
+import json
+
 from services.dependentservice import DependentService
 from model.metric import Metric
 from model.error import Errors
@@ -9,12 +11,25 @@ class MetricsService(DependentService):
         self.path = path
 
     def set_metrics(self, metrics):
-        service_url = "%s%s%s/" % (self.host, self.path, id)
-        response, status = self.post(url=service_url, content=metrics)
+        service_url = "%s%s" % (self.host, self.path)
+        response, status = self.post(url=service_url, content=metrics.build_json())
+        print('set_metrics: metrics %s' % metrics)
+        print('set_metrics: %s' % status)
         if status:
+            print('set_metrics %s' % response)
+            if 'clinic_id' in response and 'clinic_name' in response:
+                return Metric(id=response["id"],
+                              clinic_id=response["clinic_id"],
+                              clinic_name=response["clinic_name"],
+                              physician_id=response["physician_id"],
+                              physician_name=response["physician_name"],
+                              physician_crm=response["physician_crm"],
+                              patient_id=response["patient_id"],
+                              patient_name=response["patient_name"],
+                              patient_email=response["patient_email"],
+                              patient_phone=response["patient_phone"],
+                              prescription_id=metrics.prescription_id), True
             return Metric(id=response["id"],
-                          clinic_id=response["clinic_id"],
-                          clinic_name=response["clinic_name"],
                           physician_id=response["physician_id"],
                           physician_name=response["physician_name"],
                           physician_crm=response["physician_crm"],
@@ -22,7 +37,7 @@ class MetricsService(DependentService):
                           patient_name=response["patient_name"],
                           patient_email=response["patient_email"],
                           patient_phone=response["patient_phone"],
-                          prescription_id=response["prescription_id"]), True
+                          prescription_id=metrics.prescription_id), True
         if response["error"]["code"] == 400:
             return Errors.MALFORMED_REQUEST, False
         if response["error"]["code"] == 503:
