f476abc866b439b746bb3bad6182bdb25ed32589
Joaci Morais <jmorais@vtinova.com.br>
2022-01-20 11:08:15 -0400

    feat(database model): create database model

---
+++ database/model.py
@@ -0,0 +1,18 @@
+from peewee import *
+from database.connector import get_connector
+import datetime
+from model.prescription import Prescription
+
+
+class BaseModel(Model):
+    class Meta:
+        database = get_connector()
+
+
+class Prescriptions_t(BaseModel):
+    id = IntegerField(unique=True, index=True, primary_key=True)
+    clinic_id = IntegerField(default=0)
+    physician_id = IntegerField()
+    patient_id = IntegerField()
+    text = TextField()
+    metric_id = TextField(default="")
