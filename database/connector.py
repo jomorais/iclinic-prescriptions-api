40307ef0bf2be1bfd351204afe0faa6ec4450edf
Joaci Morais <jmorais@vtinova.com.br>
2022-01-21 15:14:24 -0400

    feat(mysql): add mysql connector

--- database/connector.py
+++ database/connector.py
@@ -1,9 +1,26 @@
-from peewee import SqliteDatabase
+from peewee import SqliteDatabase, MySQLDatabase
+
+DEFAULT_BTBASE_SETTINGS = {
+  "database_name": "iclinic",
+  "connector": "mysql",
+  "host": "db",
+  "user": "root",
+  "password": "iclinic",
+  "port": 3306
+}
 
 
 def sqlite_connector():
     return SqliteDatabase('Prescriptions.sqlite')
 
 
+def mysql_connector():
+    return MySQLDatabase(DEFAULT_BTBASE_SETTINGS['database_name'],
+                         user=DEFAULT_BTBASE_SETTINGS['user'],
+                         password=DEFAULT_BTBASE_SETTINGS['password'],
+                         host=DEFAULT_BTBASE_SETTINGS['host'],
+                         port=DEFAULT_BTBASE_SETTINGS['port'])
+
+
 def get_connector():
-    return sqlite_connector()
+    return mysql_connector()
