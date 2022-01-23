aa6795046985b38d9c165259c9aaca1b06ddee39
Joaci Morais <jmorais@vtinova.com.br>
2022-01-23 11:03:18 -0400

    fix(physiciansservice): add a missing erro handlings and improve code style
    Add a missing erro handlings in get_physician() method.
    
    Change name of parameter "id" to physician_id.
    
    Fix test cases due changes of error handling.

--- services/physiciansservice.py
+++ services/physiciansservice.py
@@ -1,6 +1,7 @@
 from services.dependentservice import DependentService
 from model.physician import Physician
-from model.error import Errors
+from model.error import Errors, iClinicErrors
+from utils.tools import validate_json
 
 
 class PhysiciansService(DependentService):
@@ -8,14 +9,20 @@ class PhysiciansService(DependentService):
         super().__init__(host, auth_token, timeout, retries, cache_ttl)
         self.path = path
 
-    def get_physician(self, id):
-        service_url = "%s%s%s/" % (self.host, self.path, id)
+    def get_physician(self, physician_id):
+        service_url = "%s%s%s/" % (self.host, self.path, physician_id)
         response, status = self.get(url=service_url)
-        if status:
-            return Physician(id=response["id"], name=response["name"], crm=response["crm"]), True
-        if response["error"]["code"] == 404:
-            return Errors.PHYSICIAN_NOT_FOUND, False
-        if response["error"]["code"] == 503:
-            return Errors.PHYSICIANS_SERVICE_NOT_AVAILABLE, False
+        if not status:
+            if response["error"]["code"] == 404:
+                return iClinicErrors.PHYSICIAN_NOT_FOUND, False
+            if response["error"]["code"] == 503:
+                return iClinicErrors.PHYSICIANS_SERVICE_NOT_AVAILABLE, False
+            Errors.HTTP_STATUS.code = response["error"]["code"]
+            Errors.HTTP_STATUS.http_code = Errors.HTTP_STATUS.code
+            Errors.HTTP_STATUS.message = response["error"]["message"]
+            return Errors.HTTP_STATUS, False
+        if validate_json(json_object=response, schema=Physician.schema) is False:
+            return iClinicErrors.INVALID_PHYSICIANS_SERVICE_RESPONSE, False
+        return Physician(id=response["id"], name=response["name"], crm=response["crm"]), True
 
 
