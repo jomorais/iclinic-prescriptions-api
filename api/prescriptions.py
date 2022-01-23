bf8f321a518fbc58910be7f397fb1c6cff697b20
Joaci Morais <jmorais@vtinova.com.br>
2022-01-22 23:33:54 -0400

    fix(error): fix error codes returned from api during a prescriptions request

--- api/prescriptions.py
+++ api/prescriptions.py
@@ -8,6 +8,7 @@ from utils.tools import validate_json
 from model.error import Errors
 from database.database import DatabaseStatus
 from model.http import HttpResponse
+from services.settings import dependent_services_settings
 import logging
 
 
@@ -15,35 +16,53 @@ class Prescriptions:
     def __init__(self, database):
         logging.basicConfig(format='%(levelname)s:%(message)s', level=logging.INFO)
         self.database = database
-        self.clinics_service = ClinicsService(host="https://mock-api-challenge.dev.iclinic.com.br",
-                                              path="/clinics/", retries=3, timeout=5, cache_ttl=259200,
-                                              auth_token="Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyLCJzZXJ2aWNlIjoiY2xpbmljcyJ9.r3w8KS4LfkKqZhOUK8YnIdLhVGJEqnReSClLCMBIJRQ")
-        self.patients_service = PatientsService(host="https://mock-api-challenge.dev.iclinic.com.br",
-                                                path="/patients/", retries=2, timeout=3, cache_ttl=43200,
-                                                auth_token="Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyLCJzZXJ2aWNlIjoicGF0aWVudHMifQ.Pr6Z58GzNRtjX8Y09hEBzl7dluxsGiaxGlfzdaphzVU")
-        self.physicians_service = PhysiciansService(host="https://mock-api-challenge.dev.iclinic.com.br",
-                                                    path="/physicians/", retries=2, timeout=4, cache_ttl=172800,
-                                                    auth_token="Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyLCJzZXJ2aWNlIjoicGh5c2ljaWFucyJ9.Ei58MtFFGBK4uzpxwnzLxG0Ljdd-NQKVcOXIS4UYJtA")
-        self.metrics_service = MetricsService(host="https://mock-api-challenge.dev.iclinic.com.br",
-                                              path="/metrics/", retries=5, timeout=6, cache_ttl=0,
-                                              auth_token="Bearer SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c")
+        self.clinics_service = ClinicsService(host=dependent_services_settings["clinics-service"]["host"],
+                                              path=dependent_services_settings["clinics-service"]["path"],
+                                              retries=dependent_services_settings["clinics-service"]["retries"],
+                                              timeout=dependent_services_settings["clinics-service"]["timeout"],
+                                              cache_ttl=dependent_services_settings["clinics-service"]["cache_ttl"],
+                                              auth_token=dependent_services_settings["clinics-service"]["auth_token"])
+
+        self.patients_service = PatientsService(host=dependent_services_settings["patients-service"]["host"],
+                                                path=dependent_services_settings["patients-service"]["path"],
+                                                retries=dependent_services_settings["patients-service"]["retries"],
+                                                timeout=dependent_services_settings["patients-service"]["timeout"],
+                                                cache_ttl=dependent_services_settings["patients-service"]["cache_ttl"],
+                                                auth_token=dependent_services_settings["patients-service"]["auth_token"])
+
+        self.physicians_service = PhysiciansService(host=dependent_services_settings["physicians-service"]["host"],
+                                                    path=dependent_services_settings["physicians-service"]["path"],
+                                                    retries=dependent_services_settings["physicians-service"]["retries"],
+                                                    timeout=dependent_services_settings["physicians-service"]["timeout"],
+                                                    cache_ttl=dependent_services_settings["physicians-service"]["cache_ttl"],
+                                                    auth_token=dependent_services_settings["physicians-service"]["auth_token"])
+
+        self.metrics_service = MetricsService(host=dependent_services_settings["metrics-service"]["host"],
+                                              path=dependent_services_settings["metrics-service"]["path"],
+                                              retries=dependent_services_settings["metrics-service"]["retries"],
+                                              timeout=dependent_services_settings["metrics-service"]["timeout"],
+                                              cache_ttl=dependent_services_settings["metrics-service"]["cache_ttl"],
+                                              auth_token=dependent_services_settings["metrics-service"]["auth_token"])
 
     def create_prescription(self, prescription_info):
         if validate_json(json_object=prescription_info, schema=Prescription.schema) is False:
-            return HttpResponse(json=Errors.MALFORMED_REQUEST.build_json(), code=400)
+            logging.error("Prescriptions.create_prescription(): Malformed prescription_info: %s" % prescription_info)
+            return HttpResponse(json=Errors.MALFORMED_REQUEST.build_json(), code=Errors.MALFORMED_REQUEST.http_code)
 
+        logging.info("Prescriptions.create_prescription(): creating a new prescription: %s" % prescription_info)
+        logging.info("Prescriptions.create_prescription(): accessing Dependent Services...")
         # collect Physician info from Physicians Service API
         physician, status = self.physicians_service.get_physician(id=prescription_info['physician']['id'])
         if not status:
             logging.error("Prescriptions.create_prescription(): physicians_service.get_physician(): %s" % physician.build_json())
-            return HttpResponse(json=physician.build_json(), code=400)
+            return HttpResponse(json=physician.build_json(), code=physician.http_code)
         logging.info("Prescriptions.create_prescription(): Physician acquired from Physicians Service API: %s" % physician.build_json())
 
         # collect Patient info from Patients Service API
         patient, status = self.patients_service.get_patient(id=prescription_info['patient']['id'])
         if not status:
             logging.error("Prescriptions.create_prescription(): patients_service.get_patient(): %s" % patient.build_json())
-            return HttpResponse(json=patient.build_json(), code=400)
+            return HttpResponse(json=patient.build_json(), code=patient.http_code)
         logging.info("Prescriptions.create_prescription(): Patient acquired from Patient Service API: %s" % patient.build_json())
 
         metric = Metric()
@@ -52,7 +71,6 @@ class Prescriptions:
         if status:
             logging.info("Prescriptions.create_prescription(): Clinic acquired from Clinics Service API: %s" % clinic.build_json())
             metric.set_clinic(clinic)
-            print(clinic)
         else:
             logging.warning("Prescriptions.create_prescription(): Clinic not found: %s" % clinic.build_json())
 
@@ -70,7 +88,7 @@ class Prescriptions:
         status, registered_prescription = self.database.register_prescription(prescription)
         if status == DatabaseStatus.REGISTER_PRESCRIPTION_ERROR:
             logging.error("Prescriptions.create_prescription(): cant save prescription to database")
-            return HttpResponse(json=Errors.DATABASE_ERROR.build_json(), code=400)
+            return HttpResponse(json=Errors.DATABASE_ERROR.build_json(), code=Errors.DATABASE_ERROR.http_code)
 
         logging.info("Prescriptions.create_prescription(): SAVED! prescription.id: %s" % registered_prescription.id)
 
@@ -86,9 +104,9 @@ class Prescriptions:
             status, removed_prescription = self.database.remove_prescription(registered_prescription.id)
             if status == DatabaseStatus.REMOVE_PRESCRIPTION_ERROR:
                 logging.error("Prescriptions.create_prescription(): cant remove prescription from database")
-                return HttpResponse(json=Errors.DATABASE_ERROR.build_json(), code=400)
+                return HttpResponse(json=Errors.DATABASE_ERROR.build_json(), code=Errors.DATABASE_ERROR.http_code)
             logging.error("Prescriptions.create_prescription(): rollback is done")
-            return HttpResponse(json=metrics.build_json(), code=400)
+            return HttpResponse(json=metrics.build_json(), code=metrics.http_code)
 
         logging.info("Prescriptions.create_prescription(): Metrics are integrated!! Metrics.id: %s" % metrics.id)
 
@@ -98,7 +116,7 @@ class Prescriptions:
         status, new_prescription = self.database.update_prescription(registered_prescription)
         if status == DatabaseStatus.UPDATE_PRESCRIPTION_ERROR:
             logging.info("Prescriptions.create_prescription(): Metrics.id couldn't applied due DATABASE ERROR")
-            return HttpResponse(json=Errors.DATABASE_ERROR.build_json(), code=400)
+            return HttpResponse(json=Errors.DATABASE_ERROR.build_json(), code=Errors.DATABASE_ERROR.http_code)
 
         logging.info("Prescriptions.create_prescription(): New Prescription are created successfully!!")
         logging.info("Prescriptions.create_prescription(): New Prescription: %s" % new_prescription.build_json())
--- model/error.py
+++ model/error.py
@@ -1,40 +1,26 @@
 from dataclasses import dataclass
 
-"""
-01	malformed request
-02	physician not found
-03	patient not found
-04	metrics service not available
-05	physicians service not available
-06	patients service not available
-07  clinic not found
-08  clinics service not available
-09  request timeout
-10  http error
-"""
-
-
 class ErrorCode:
-    MALFORMED_REQUEST = 1
-    PHYSICIAN_NOT_FOUND = 2
-    PATIENT_NOT_FOUND = 3
-    METRICS_SERVICE_NOT_AVAILABLE = 4
-    PHYSICIANS_SERVICE_NOT_AVAILABLE = 5
-    PATIENTS_SERVICE_NOT_AVAILABLE = 6
-    CLINIC_NOT_FOUND = 7
-    CLINICS_SERVICE_NOT_AVAILABLE = 8
-    REQUEST_TIMEOUT = 9
-    HTTP_ERROR = 10
-    INVALID_URL = 11
-    HTTP_STATUS = 12
-    DATABASE_ERROR = 13
-    PRESCRIPTION_NOT_FOUND = 14
+    CODE_MALFORMED_REQUEST = 1
+    CODE_PHYSICIAN_NOT_FOUND = 2
+    CODE_PATIENT_NOT_FOUND = 3
+    CODE_METRICS_SERVICE_NOT_AVAILABLE = 4
+    CODE_PHYSICIANS_SERVICE_NOT_AVAILABLE = 5
+    CODE_PATIENTS_SERVICE_NOT_AVAILABLE = 6
+    CODE_CLINIC_NOT_FOUND = 7
+    CODE_CLINICS_SERVICE_NOT_AVAILABLE = 8
+    CODE_REQUEST_TIMEOUT = 9
+    CODE_HTTP_ERROR = 10
+    CODE_INVALID_URL = 11
+    CODE_HTTP_STATUS = 12
+    CODE_DATABASE_ERROR = 13
 
 
 @dataclass
 class Error:
     message: str = ''
     code: int = 0
+    http_code: int = 400
 
     def build_json(self, message='', code=0):
         if message != '':
@@ -47,27 +33,38 @@ class Error:
 @dataclass
 class Errors:
     MALFORMED_REQUEST: Error = Error(message="malformed request",
-                                     code=ErrorCode.MALFORMED_REQUEST)
+                                     code=ErrorCode.CODE_MALFORMED_REQUEST,
+                                     http_code=400)
     PHYSICIAN_NOT_FOUND: Error = Error(message="physician not found",
-                                       code=ErrorCode.PHYSICIAN_NOT_FOUND)
+                                       code=ErrorCode.CODE_PHYSICIAN_NOT_FOUND,
+                                       http_code=404)
     PATIENT_NOT_FOUND: Error = Error(message="patient not found",
-                                     code=ErrorCode.PATIENT_NOT_FOUND)
+                                     code=ErrorCode.CODE_PATIENT_NOT_FOUND,
+                                     http_code=404)
     METRICS_SERVICE_NOT_AVAILABLE: Error = Error(message="metrics service not available",
-                                                 code=ErrorCode.METRICS_SERVICE_NOT_AVAILABLE)
+                                                 code=ErrorCode.CODE_METRICS_SERVICE_NOT_AVAILABLE,
+                                                 http_code=503)
     PHYSICIANS_SERVICE_NOT_AVAILABLE: Error = Error(message="physicians service not available",
-                                                    code=ErrorCode.PHYSICIANS_SERVICE_NOT_AVAILABLE)
+                                                    code=ErrorCode.CODE_PHYSICIANS_SERVICE_NOT_AVAILABLE,
+                                                    http_code=503)
     PATIENTS_SERVICE_NOT_AVAILABLE: Error = Error(message="patients service not available",
-                                                  code=ErrorCode.PATIENTS_SERVICE_NOT_AVAILABLE)
+                                                  code=ErrorCode.CODE_PATIENTS_SERVICE_NOT_AVAILABLE,
+                                                  http_code=503)
     CLINIC_NOT_FOUND: Error = Error(message="clinic not found",
-                                    code=ErrorCode.CLINIC_NOT_FOUND)
+                                    code=ErrorCode.CODE_CLINIC_NOT_FOUND,
+                                    http_code=404)
     CLINICS_SERVICE_NOT_AVAILABLE: Error = Error(message="clinics service not available",
-                                                 code=ErrorCode.CLINICS_SERVICE_NOT_AVAILABLE)
+                                                 code=ErrorCode.CODE_CLINICS_SERVICE_NOT_AVAILABLE,
+                                                 http_code=503)
     REQUEST_TIMEOUT: Error = Error(message="request timeout",
-                                   code=ErrorCode.REQUEST_TIMEOUT)
+                                   code=ErrorCode.CODE_REQUEST_TIMEOUT,
+                                   http_code=408)
     HTTP_ERROR: Error = Error(message="http error",
-                              code=ErrorCode.HTTP_ERROR)
+                              code=ErrorCode.CODE_HTTP_ERROR,
+                              http_code=500)
     INVALID_URL: Error = Error(message="invalid url",
-                               code=ErrorCode.INVALID_URL)
+                               code=ErrorCode.CODE_INVALID_URL,
+                               http_code=400)
     HTTP_STATUS: Error = Error(message="http status")
-    DATABASE_ERROR: Error = Error(message="database error", code=ErrorCode.DATABASE_ERROR)
-    PRESCRIPTION_NOT_FOUND: Error = Error(message="prescription not found", code=ErrorCode.PRESCRIPTION_NOT_FOUND)
+    DATABASE_ERROR: Error = Error(message="database error", code=ErrorCode.CODE_DATABASE_ERROR,
+                                  http_code=500)
