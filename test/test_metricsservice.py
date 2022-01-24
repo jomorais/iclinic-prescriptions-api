51dbbea0768d6efbb467b7dc1dde4d3f0adf9f83
Joaci Morais <jmorais@vtinova.com.br>
2022-01-23 21:14:02 -0400

    test(metricsservice): add missing test cases

--- test/test_metricsservice.py
+++ test/test_metricsservice.py
@@ -103,3 +103,92 @@ def test_metricsservice_set_metrics_service_not_available():
     assert response.message == iClinicErrors.METRICS_SERVICE_NOT_AVAILABLE.message
     assert response.code == iClinicErrors.METRICS_SERVICE_NOT_AVAILABLE.code
 
+
+def test_metricsservice_set_metrics_http_status():
+    def setup():
+        metrics = Metric(clinic_id=1,
+                         clinic_name="Clinica A",
+                         physician_id=1,
+                         physician_name="José",
+                         physician_crm="SP293893",
+                         patient_id=1,
+                         patient_name="Rodrigo",
+                         patient_email="rodrigo@gmail.com",
+                         patient_phone="(16)998765625",
+                         prescription_id=1)
+
+        class MockedRequester:
+            def post(self, url, content):
+                return Errors.HTTP_STATUS.build_json(code=203), False
+
+        ms = MetricsService(host="https://mock-api-challenge.dev.iclinic.com.br",
+                            path="/metrics/", retries=5, timeout=6, cache_ttl=0,
+                            auth_token="Bearer SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c")
+        ms.requester = MockedRequester()
+        return metrics, ms
+    metrics, ms = setup()
+    response, status = ms.set_metrics(metrics=metrics)
+    assert not status
+    assert type(response) == Error
+    assert response.message == Errors.HTTP_STATUS.message
+    assert response.code == Errors.HTTP_STATUS.code
+
+
+def test_metricsservice_set_metrics_validate_json():
+    def setup():
+        metrics = Metric(clinic_id=1,
+                         clinic_name="Clinica A",
+                         physician_id=1,
+                         physician_name="José",
+                         physician_crm="SP293893",
+                         patient_id=1,
+                         patient_name="Rodrigo",
+                         patient_email="rodrigo@gmail.com",
+                         patient_phone="(16)998765625",
+                         prescription_id=1)
+
+        class MockedRequester:
+            def post(self, url, content):
+                return {"id": 123}, True
+
+        ms = MetricsService(host="https://mock-api-challenge.dev.iclinic.com.br",
+                            path="/metrics/", retries=5, timeout=6, cache_ttl=0,
+                            auth_token="Bearer SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c")
+        ms.requester = MockedRequester()
+        return metrics, ms
+    metrics, ms = setup()
+    response, status = ms.set_metrics(metrics=metrics)
+    assert not status
+    assert type(response) == Error
+    assert response.message == iClinicErrors.INVALID_METRICS_SERVICE_RESPONSE.message
+    assert response.code == iClinicErrors.INVALID_METRICS_SERVICE_RESPONSE.code
+
+
+def test_metricsservice_set_metrics_metrics_without_clinic_info():
+    def setup():
+        metrics = Metric(physician_id=1,
+                         physician_name="José",
+                         physician_crm="SP293893",
+                         patient_id=1,
+                         patient_name="Rodrigo",
+                         patient_email="rodrigo@gmail.com",
+                         patient_phone="(16)998765625",
+                         prescription_id=1)
+
+        class MockedRequester:
+            def post(self, url, content):
+                return metrics.build_json(), True
+
+        ms = MetricsService(host="https://mock-api-challenge.dev.iclinic.com.br",
+                            path="/metrics/", retries=5, timeout=6, cache_ttl=0,
+                            auth_token="Bearer SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c")
+        ms.requester = MockedRequester()
+        return metrics, ms
+    metrics, ms = setup()
+    response, status = ms.set_metrics(metrics=metrics)
+    assert status
+    assert "clinic_id" not in metrics.build_json()
+    assert "clinic_name" not in metrics.build_json()
+
+
+
